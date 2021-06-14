aa8dda85d4eb083580e1c3fabbf526297d9580ec
loveseo8 <maggaket@gmail.com>
2021-06-14 16:22:57 +0300

    +change brush size +eraser +change brush color

--- app/src/main/java/com/aad/drawingapp/MainActivity.java
+++ app/src/main/java/com/aad/drawingapp/MainActivity.java
@@ -1,15 +1,22 @@
 package com.aad.drawingapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.graphics.Path;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+
+import top.defaults.colorpicker.ColorPickerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int currentColor = Color.BLACK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,22 +45,85 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
-            final Button increase = findViewById(R.id.increase_brush_size);
-            increase.setOnClickListener(new View.OnClickListener() {
+            final Button changeBrushSize = findViewById(R.id.change_brush_size);
+            changeBrushSize.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
 
-                    paintView.brushSize++;
+                    AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                    LayoutInflater layoutInflater = getLayoutInflater();
+                    final View dialogView = layoutInflater.inflate(R.layout.change_size_dialog, null);
+                    dialogBuilder.setView(dialogView);
+
+
+                    final Button buttonChange = dialogView.findViewById(R.id.change_button);
+                    SeekBar seekBar = dialogView.findViewById(R.id.seekBar);
+
+                    seekBar.setProgress(paintView.brushSize);
+
+                    seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                        @Override
+                        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                            paintView.brushSize = progress;
+
+                        }
+
+                        @Override
+                        public void onStartTrackingTouch(SeekBar seekBar) {
+
+                        }
+
+                        @Override
+                        public void onStopTrackingTouch(SeekBar seekBar) {
+
+                        }
+                    });
+
+                    final AlertDialog b = dialogBuilder.create();
+                    b.show();
+
+                    buttonChange.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                            b.dismiss();
+
+                        }
+                    });
 
                 }
             });
 
-            final Button decrease = findViewById(R.id.decrease_brush_size);
-            decrease.setOnClickListener(new View.OnClickListener() {
+            final Button changeBrushColor = findViewById(R.id.change_brush_color);
+            changeBrushColor.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
 
-                    if(paintView.brushSize > 1) paintView.brushSize--;
+                    AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                    LayoutInflater layoutInflater = getLayoutInflater();
+                    final View dialogView = layoutInflater.inflate(R.layout.change_color_dialog, null);
+                    dialogBuilder.setView(dialogView);
+
+
+                    final Button buttonChange = dialogView.findViewById(R.id.change_button);
+                    ColorPickerView colorPickerView = dialogView.findViewById(R.id.colorPickerView);
+
+
+                    final AlertDialog b = dialogBuilder.create();
+                    b.show();
+
+                    buttonChange.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                            paintView.brushColor = colorPickerView.getColor();
+                            currentColor = colorPickerView.getColor();
+
+                            b.dismiss();
+
+                        }
+                    });
 
                 }
             });
@@ -63,8 +133,8 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
 
-                    if(paintView.brushColor == Color.BLACK) paintView.brushColor = Color.WHITE;
-                    else paintView.brushColor = Color.BLACK;
+                    if(!(paintView.brushColor == Color.WHITE)) paintView.brushColor = Color.WHITE;
+                    else paintView.brushColor = currentColor;
 
                 }
             });
